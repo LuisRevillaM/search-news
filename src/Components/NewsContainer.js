b7c20a222d5e71acc13f1386ee652ce413719444
Luis Revilla <luisrevillameza@gmail.com>
2019-01-18 09:09:22 -0500

    moved api-key from url to request headers

--- src/Components/NewsContainer.js
+++ src/Components/NewsContainer.js
@@ -44,12 +44,16 @@ class NewsContainer extends Component {
   controller;
   signal;
   fetchNews;
+  buildHeader = () => {
+    const apiKeyHeader = { "X-Api-Key": "7fa0c8e603034cadbb9f36b4f8c21c87" };
+    const headers = new Headers(apiKeyHeader);
+    return headers;
+  };
   buildUrl = (searchTerm, sortCriteria) => {
     const myParams = {
       language: this.props.language || "en",
       q: searchTerm,
-      sortBy: sortCriteria || "",
-      apiKey: "7fa0c8e603034cadbb9f36b4f8c21c87"
+      sortBy: sortCriteria || ""
     };
 
     const url = new URL("https://newsapi.org/v2/everything");
@@ -60,12 +64,13 @@ class NewsContainer extends Component {
     const dispatch = this.dispatch(this.stateReducer, this.state);
     const signal = this.signal;
     const buildUrl = this.buildUrl;
+    const headers = this.buildHeader();
 
     return async function(searchTerm, sortCriteria) {
       if (searchTerm.length > 0) {
         const url = buildUrl(searchTerm, sortCriteria);
         dispatch({ type: "FETCH_NEWS" });
-        let data = await fetch(url, { signal });
+        let data = await fetch(url, { signal, headers });
         let jsonData = await data.json();
         dispatch({ type: "NEWS_ARRIVED", payload: jsonData.articles });
       }
